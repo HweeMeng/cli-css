ebc0c3c2e07055ab3db6eca1a6db91bb288fe661
lekhweemeng <lek.hweemeng@live.com>
2020-04-02 22:58:30 +0800

    completed rgb to hex file. completed separation of files right from the start

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var hexToRGB = require("./hex-to-RGB.js");
+var rgbToHex = require("./rgb-to-hex.js");
 
 
 if (process.argv[2] == "hex"){
     console.log("RGB: " + hexToRGB(process.argv[3]).r + "," + hexToRGB(process.argv[3]).g + ","
         + hexToRGB(process.argv[3]).b);
+}else if (process.argv[2] == "rgb"){
+    console.log(rgbToHex(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
 }
